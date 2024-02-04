2dd700abb7d3d1d1d91b3655bf1a86491a167d08
weakson <950823eason@gmail.com>
2024-02-04 08:18:45 +0800

    edit tabstop

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 
 int main(){
-
     int n;
     cin >> n;
 
-    vector<int> arr (n);
+    vector<int> arr(n);
 
-	for (int i = 0; i < n; i++){
-		for (int j = 0; j < n - i; j++){
-			if (arr[j] > arr[j + 1]){
-				swap (arr[j], arr[j + 1]);
-			}
-		}
-	}
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n - i; j++){
+            if (arr[j] > arr[j + 1]){
+                swap (arr[j], arr[j + 1]);
+            }
+        }
+    }
 
-	for (auto i : arr) cout << i << ' ';
+    for (auto i : arr) cout << i << ' ';
+    cout << '\n';
+
+    return 0;
 }
