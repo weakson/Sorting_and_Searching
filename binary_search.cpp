9e256ca832110a4ddee9d3702932d0c6ecd692ab
weakson <950823eason@gmail.com>
2024-02-05 10:54:34 +0800

    edit binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -10,22 +10,22 @@ int main(){
     for (int i = 0; i < n; i++) cin >> arr[i];
     sort (arr, arr + n);
 
-    int l = 0, r = n - 1;
-    int ans = -1;
+    int l = 0, r = n;
 
-    while (l <= r){
+    while (l < r){
         int mid = (l + r) / 2;
-        if (target < arr[mid]){
-            r = mid - 1;
+        if (arr[mid] > target){
+            r = mid;
         }
         else if (arr[mid] < target){
             l = mid + 1;
         }
         else{
-            ans = mid;
+            l = mid;
             break;
         }
     }
 
-    cout << ans << '\n';
+    if (arr[l] != target) cout << "Not found\n";
+    else cout << target << " is inside the array\n";
 }
