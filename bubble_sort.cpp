ec79286318003b34663bbce5c3073e2f9671a542
吳冠昱 <102860739+weakson@users.noreply.github.com>
2025-04-18 17:22:05 +0800

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -9,7 +9,7 @@ int main(){
     for (int i = 0; i < n; i++) cin >> arr[i];
 
     for (int i = 0; i < n; i++){
-        for (int j = 0; j < n - i; j++){
+        for (int j = 0; j < n - i - 1; j++){
             if (arr[j] > arr[j + 1]){
                 swap (arr[j], arr[j + 1]);
             }
